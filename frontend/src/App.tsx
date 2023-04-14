8cbe0ecf25d48ebd2bf1299c74fd744016d364ee
Deacon Hughes <deaconhughes07@gmail.com>
2023-04-14 14:27:48 +0100

    got tailwind working figured out darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="bg-white text-black dark:bg-black dark:text-white">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
